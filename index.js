9ac33da5b014326740f14d6d3cd0d6527614ad3c
Aisha Hussein <aishasalaam07@gmail.com>
2023-03-14 08:39:16 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+// function multiply(num){
+
+// }
+
+    let num1= 31;
+    let num2= 2;
+    let num3 = 22;
+    let num4 = 9;
+   let multiply = num1 * num2;
+    
+    // console.log('num1 * num2');
+    let random = Math.floor(Math.random()*10)+1;
+
+  let mod = num3 % num4;
+  let max = Math.max (5,8,9,20);
